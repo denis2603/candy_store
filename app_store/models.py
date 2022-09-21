813867b84b0665c66095a05c1d909bfae757d378
Denis Zheltikov <den.ari@yandex.ru>
2022-09-21 13:44:04 +0300

    Create models

--- app_store/models.py
+++ app_store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+
+    title = models.CharField(max_length=150, unique=True, verbose_name='наименование категории')
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey(Categories, on_delete=models.RESTRICT, verbose_name='категория')
+    name = models.CharField(max_length=150, verbose_name='наименование продукта')
+    description = models.TextField(verbose_name='описание')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='цена')
+    foto = models.ImageField(upload_to='foto/', verbose_name='изображение')
+
---
+++ vk_bot/services.py
@@ -0,0 +1,12 @@
+import vk_api
+
+
+class BotVk:
+
+    def __init__(self):
+        pass
+
+
+    def add_market(self):
+        pass
+
